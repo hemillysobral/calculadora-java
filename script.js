d3b6282d1240ac1b1214616bd2ab15f50d345e24
hs <hemillysobral53@gmail.com>
2025-09-14 20:28:04 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,19 @@
-function appendNumber(num) {
-  const display = document.getElementById('display');
-  display.value = display.value + num;
-}
-
-function appendOperator(op) {
-  const display = document.getElementById('display');
-  display.value = display.value + op;
+function append(value) {
+    document.getElementById('display').value += value;
 }
 
 function clearDisplay() {
-  document.getElementById('display').value = '';
+    document.getElementById('display').value = '';
 }
 
-function calculateResult() {
-  const expression = document.getElementById('display').value;
-
-  fetch('/api/calcular', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ expressao: expression })
-  })
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById('display').value = data.resultado;
-  })
-  .catch(() => {
-    document.getElementById('display').value = 'Erro';
-  });
+function calculate() {
+    const expression = document.getElementById('display').value;
+    fetch('/calc?expr=' + encodeURIComponent(expression))
+        .then(res => res.text())
+        .then(result => {
+            document.getElementById('display').value = result;
+        })
+        .catch(() => {
+            document.getElementById('display').value = 'Erro';
+        });
 }
